65e857bae1f99f5f91bdf77a3869077053e020db
Lucas <lteixeira507@gmail.com>
2025-04-07 14:17:19 -0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'Calculadora-de-Trocado', 
   server: {
     port: 3000,
-    host: true,
-    open: true
+    open: true,
   },
-  preview: {
-    port: 3000
-  }
 });
